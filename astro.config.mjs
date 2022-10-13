5d475a8174b3c19249b3cc9572a026b02b60112c
Mathieu Dhondt <laundro@gmail.com>
2022-10-13 22:37:44 +0200

    Base astro project

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import {
+  defineConfig
+} from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    rehypePlugins: [
+      setFallbackLayout
+    ]
+  },
+});
+
+function setFallbackLayout() {
+  // sets a default layout for all md/mdx files
+  return function (_tree, file) {
+    const layout =
+      file.data.astro.frontmatter.layout ?? "./src/layouts/Layout.astro";
+    file.data.astro.frontmatter.layout = layout;
+  };
+}
